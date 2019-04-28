9bd091b05ad692eeb7b165658e5c0fe8e6ba0f87
Shoptorshi Jamil <sjami002@gmail.com>
2019-04-28 20:02:12 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,45 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  
+  get '/' do
+    redirect to "/recipes"
+  end
+  
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+  
+  get '/recipes/new' do
+    @recipes = Recipe.new
+    erb :new
+  end
+  
+  post '/recipes' do
+    @recipes = Recipe.create(params)
+    redirect to "/recipes/#{@recipes.id}"
+  end
+  
+  get '/recipes/:id' do
+    @recipes = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+  
+  get '/recipes/:id/edit' do
+    @recipes = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+  
+  patch '/recipes/:id' do
+    @recipes = Recipe.find_by_id(params[:id])
+    @recipes.update(params[:recipe])
+    redirect to "/recipes/#{@recipes.id}"
+  end
+  
+  delete '/recipes/:id' do
+    Recipe.destroy(params[:id])
+  end
+  
 
 end
